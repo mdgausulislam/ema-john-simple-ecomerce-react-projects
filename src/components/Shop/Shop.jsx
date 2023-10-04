ec1eb0c88ad03806bf0bdba90d613a97d5c9c61d
unknown <gausulislam5@gmail.com>
2023-10-05 00:02:46 +0600

    update local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,7 +7,7 @@ import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart,setCart]=useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
@@ -16,22 +16,40 @@ const Shop = () => {
 
     }, []);
 
-    useEffect(()=>{
-        const storedCart=getShoppingCart();
+    // useEffect(()=>{
+    //     const storedCart=getShoppingCart();
 
-        //step-01  get id
+    //     //step-01  get id
+    //     for (const id in storedCart) {
+    //         // step-02 get the product using id
+    //             const addedProduct=products.find(product=>product.id===id);
+    //             //step-03 get the quantity of the product
+    //             const quantity=storedCart[id];
+    //             addedProduct.quantity=quantity;
+    //     }
+
+    // },[products]);
+
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const saveCart=[];
+
         for (const id in storedCart) {
-            // step-02 get the product using id
-                const addedProduct=products.find(product=>product.id===id);
-                //step-03 get the quantity of the product
-                const quantity=storedCart[id];
-                addedProduct.quantity=quantity;
+            const addedProduct = products.map(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+            }
+           
+
         }
+        setCart(saveCart)
+    }, [products])
 
-    },[]);
-
-    const handleAddToCart=(product)=>{
-        const newCart=[...cart,product];
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id);
     }
@@ -40,17 +58,17 @@ const Shop = () => {
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product=><Product
-                    key={product.id}
-                    product={product}
-                    handleAddToCart={handleAddToCart}
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
 
             <div className="cart-container">
                 <Cart
-                cart={cart}
+                    cart={cart}
                 ></Cart>
             </div>
         </div>
