97eca2355068d8f3275b4185a4e70e45fdc86927
unknown <gausulislam5@gmail.com>
2023-10-04 11:55:58 +0600

    update btn and margin

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price} = props.product;
+    const { img, name, seller, ratings, price } = props.product;
     return (
         <div className="product">
             <img src={img} alt="" />
-            <h5>{name}</h5>
-            <p>{seller}</p>
-            <p>{quantity}</p>
-            <p>{price}</p>
-          
+            <div className="product-info">
+                <h6 className="product-name">{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacture: {seller}</p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button className="btn-cart">Add to Cart</button>
         </div>
     );
 };
