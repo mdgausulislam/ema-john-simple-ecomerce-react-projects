0596ae48895155e76a13614d05fdac3afc4f2f9b
unknown <gausulislam5@gmail.com>
2023-10-04 12:13:13 +0600

    event handelar added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -11,7 +11,13 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
 
-    }, [])
+    }, []);
+
+
+    const handleAddToCart=(product)=>{
+        console.log(product);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -19,6 +25,7 @@ const Shop = () => {
                     products.map(product=><Product
                     key={product.id}
                     product={product}
+                    handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
