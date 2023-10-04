1358edf041d76c52f08cc724b66e2bf92ca5fdd8
unknown <gausulislam5@gmail.com>
2023-10-04 20:49:12 +0600

    update total grand price

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,18 +3,24 @@ import './Cart.css';
 
 const Cart = (props) => {
     const {cart} = props;
-    let total=0;
+    let totalPrice=0;
+    let totalShipping=0;
     for (const product of cart) {
-        total=total+product.price;
-        console.log(total);
+        totalPrice=totalPrice+product.price;
+        totalShipping=totalShipping+product.shipping;
+        console.log(totalPrice);
     }
+    const tax=totalPrice*7/100;
+
+    const grandTotal=(totalPrice+totalShipping+tax).toFixed(2);
     return (
-        <div className="cart1">
+        <div className="cart">
             <h4>Order Summary</h4>
             <p>Selected Items:{cart.length} </p>
-            <p>Total Price: ${total}</p>
-            <p>Total Shipping Charge: $</p>
-            <p>Tax: $</p>
+            <p>Total Price:${totalPrice}</p>
+            <p>Shipping Charge:${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
             <button>Clear Cart </button>
             <button>Review Order </button> 
         </div>
